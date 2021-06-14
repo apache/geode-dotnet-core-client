d699d234d9a2a9d1db0b79f19d2331132ddd934a
Mike Martell <mmartell@pivotal.io>
2021-06-14 15:38:40 -0700

    Switch to .NET 5.0

--- NetCore/Constants.cs
+++ NetCore/Constants.cs
@@ -9,7 +9,7 @@ namespace Apache
             {
               public const string libPath =
                   //"/Users/matthewreddington/workspace/apache/geode-native/cppcache/shared/libapache-geode.dylib";
-                  "c:/Users/bblake/src/nc_install/bin/apache-geode-c.dll";
+                  "C:/geode-native-c_bindings/build/c_bindings/Debug/apache-geode-c.dll";
             }
         }
     }
